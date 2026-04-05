df1ad807f002c313de4fa053979bad9272bda9e5
santilod <santilod@gmail.com>
2026-04-05 10:04:50 -0300

    borro script que no me anduvo

--- code/web-app/app/layout.tsx
+++ code/web-app/app/layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
-        <script src="https://tweakcn.com/live-preview.min.js"></script>
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
